e77d89aa7de374e5adbee675bba29e579b7a4cbe
mendozabree <mendozabridget777@gmail.com>
2019-07-17 12:20:50 +0300

    - create department model - display all departments - create a welcome page

--- app.rb
+++ app.rb
@@ -1,3 +1,14 @@
 require 'sinatra'
 require 'active_record'
 require './config/environments'
+require './models/departments.rb'
+
+class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+  get '/departments' do
+    @departments = Departments.all
+    erb :departments
+  end
+end
